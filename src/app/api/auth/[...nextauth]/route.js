c9383f54dd6c4d51086924c46e6f4c60026b6843
@juankmorcillo2 <juankmorcillo2@gmail.com>
2025-04-24 10:19:45 -0500

    feature: manejo de autenticacion y guardado de sesion del usuario

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,62 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { API_BASE_URL } from "@/lib/api/config"
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                try {
+                    const res = await fetch(API_BASE_URL + '/login', {
+                        method: 'POST',
+                        body: JSON.stringify(credentials),
+                        headers: { "Content-Type": "application/json" }
+                    })
+                    const user = await res.json()
+
+                    if (res.ok && user) {
+                        return user
+                    }
+                    return null
+                } catch (error) {
+                    console.error("Auth error:", error)
+                    return null
+                }
+            }
+        })
+    ],
+    pages: {
+        signIn: '/',
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.token
+                token.nombres = user.nombres
+                token.apellidos = user.apellidos
+                token.email = user.correo
+                token.rol = 'usuario'
+                // Puedes agregar más datos al token si es necesario
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id
+                session.user.nombres = token.nombres
+                session.user.apellidos = token.apellidos
+                session.user.email = token.email
+                // Sincroniza datos adicionales con la sesión
+            }
+            return session
+        }
+    }
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
